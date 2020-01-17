d959f3d3eac1c331d28decea6ca142e48fbd0d70
brunormferreira <brunormferreira@hotmail.com>
2020-01-16 22:16:59 -0300

    add: styles to desctructuring page

--- src/pages/destructuring/index.js
+++ src/pages/destructuring/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { graphql } from "gatsby"
+import { graphql } from 'gatsby'
 
-import { DestructuringContent, DestructuringTitle, Iframe } from './styles';
+import { DestructuringContent, DestructuringTitle, DestructuringIframe, DestructuringParagraph } from './styles';
 
 export default function Destructuring ({data}) {
   return (
     <DestructuringContent>
-      <DestructuringTitle>{data.markdownRemark.frontmatter.title}</DestructuringTitle>
-      <p>{data.markdownRemark.frontmatter.subtitle}</p>
-      <p>{data.markdownRemark.frontmatter.date}</p>
-      <Iframe>
-        <iframe width="1024px;" height="473px;"
+      <DestructuringTitle>
+        {data.markdownRemark.frontmatter.title}
+      </DestructuringTitle>
+      <DestructuringParagraph>
+        {data.markdownRemark.frontmatter.subtitle}
+      </DestructuringParagraph>
+      <DestructuringIframe>
+        <iframe width='1024px;' height='473px;' title="frame"
           src="https://carbon.now.sh/embed?bg=rgba(171%2C%20184%2C%20195%2C%201)&t=seti&wt=none&l=auto&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=4x&wm=false&code=const%2520carro%2520%253D%2520%257B%250A%2520%2520marca%253A%2520'Fiat'%252C%250A%2520%2520ano%253A%25202018%252C%250A%2520%2520portas%253A%25204%252C%250A%257D%250A%250Aconst%2520%257Bmarca%252C%2520ano%257D%2520%253D%2520carro%253B%250A%250Aconsole.log(marca)%253B%2520%252F%252F%2520Fiat%250Aconsole.log(ano)%253B%2520%252F%252F%25202018%250A">
         </iframe>
-      </Iframe>
+      </DestructuringIframe>
     </DestructuringContent>
   )
 }
--- src/pages/destructuring/styles.js
+++ src/pages/destructuring/styles.js
@@ -5,19 +5,29 @@ export const DestructuringContent = styled.div`
     float: right;
     height: 100vh;
     text-align: center;
+    font-weight: bold;
+    color: #FFFFFF;
+    background-color: #282C35;
 `;
 
 export const DestructuringTitle = styled.h1`
+    text-decoration: underline;
+    text-decoration-color: #FF9900;
     font-weight: bold;
 `;
 
-export const Iframe = styled.div`
-    transform:scale(0.7);
-    border:0; 
-    overflow:hidden;
+export const DestructuringParagraph = styled.p`
+  /* TO DO */
+`
 
+export const DestructuringIframe = styled.div`
+    transform: scale(0.7);
+    border: 0; 
+    overflow: hidden;
+    border-radius: 15px;
+    
     & iframe {
         width: 100%;
         border: none;
     }
-`; 
+`;
