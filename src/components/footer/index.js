a7e4088b8e6205d6a2fb92a2438f708c65dae054
LeonardoCesca <leonardocescaf@gmail.com>
2020-01-19 17:50:02 -0300

    update: style into footer (unfinished)

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import { FooterContent } from './styles';
+import { FooterContent, FooterText } from './styles';
 
 export default function Footer() {
   return (
     <FooterContent>
-      <h1>Footer</h1>
+      <FooterText>Made with 
+        <span aria-label="love" role="img">💖 </span>
+        by <a href="https://github.com/LeonardoCesca" rel="noopener noreferrer" target="_blank">Leonardo Cesca</a> and 
+        <a href="https://github.com/brunormferreira" rel="noopener noreferrer" target="_blank">Bruno Ramires</a>
+      </FooterText>
     </FooterContent>
   )
 }
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -4,7 +4,11 @@ import styled from 'styled-components';
 export const FooterContent = styled.footer`
     position: fixed;
     bottom: 0;
-    width: 16.3%;
+    width: 16.9%;
     background-color: #fff;
     color: #000;
+`;
+
+export const FooterText = styled.p` 
+    text-align: center;
 `;
