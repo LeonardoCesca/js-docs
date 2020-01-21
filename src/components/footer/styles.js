537511042bd3081932a8c6c113f66153985bf545
LeonardoCesca <leonardocescaf@gmail.com>
2020-01-21 15:58:20 -0300

    add: ajust footer style

--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -6,8 +6,9 @@ export const FooterContent = styled.footer`
     bottom: 0;
     width: 16.7%;
     border-right: 2px solid #FF9900;
-    background-color: #fff;
-    color: #000;
+    background-color: rgb(40, 44, 53);
+    border-top: 2px solid rgb(255, 153, 0);
+    color: #fff;
 
     @media (max-width: 768px) {
         width: 100%;
@@ -16,10 +17,13 @@ export const FooterContent = styled.footer`
 
 export const FooterText = styled.p` 
     text-align: center;
+    color: #fff;
 
     & a {
         margin-top: 10px;
         margin-bottom: 5px;
+        color: #fff;
+        font-weight: bold;
 
         &:last-child {
             margin-top: 5px;
