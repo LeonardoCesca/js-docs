72d8d501b01ad07eba8b233b16ed98981c692322
LeonardoCesca <leonardocescaf@gmail.com>
2020-01-23 09:44:19 -0300

    add: home into default page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+import { HomeContent } from './styles';
+
+export default function Home() {
+  return (
+    <HomeContent>
+      <h1>Home</h1>
+    </HomeContent>
+  )
+}
---
+++ src/pages/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HomeContent = styled.div`
+    width: 100%;
+    float: right;
+    height: 100vh;
+    text-align: center;
+    font-weight: bold;
+    color: #FFFFFF;
+    background-color: #282C35;
+    text-decoration: underline;
+    text-decoration-color: #FF9900
+`;
