e2f04cb3f7427efc15fc449ea36906ece1c460cb
brunormferreira <brunormferreira@hotmail.com>
2020-01-23 21:25:28 -0300

    fix: remove unused dependencies

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import { navigate } from 'gatsby'
+import React, { useState } from 'react'
 
 import { GlobalStyle } from '../../theme/globalStyle';
 
