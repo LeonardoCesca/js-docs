1575f2ad00d52c2bdaf0ae07a767f8f5deb00d24
LeonardoCesca <leonardocescaf@gmail.com>
2020-01-24 11:19:13 -0300

    add: global style into 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 import { Content, Title, Paragraph } from './styles';
 
+import { GlobalStyle } from '../theme/globalStyle';
+
 const NotFoundPage = () => (
   <Content>
+    <GlobalStyle />
     <Title>NOT FOUND</Title>
     <Paragraph>Página não existe :(</Paragraph>
   </Content>
