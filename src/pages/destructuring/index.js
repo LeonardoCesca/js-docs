27bff8e63fe5295448abe814b1b293a7bb753953
brunormferreira <brunormferreira@hotmail.com>
2020-01-23 21:00:52 -0300

    fix: styled link at destruct page

--- src/pages/destructuring/index.js
+++ src/pages/destructuring/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 
 import { Content, Title, Paragraph, Iframe, Links, ContentInfos } from '../styles';
+import { StyledLink } from '../../components/layout/styles';
 
 export default function Destructuring ({data}) {
   return (
@@ -24,7 +25,7 @@ export default function Destructuring ({data}) {
       <ContentInfos>
         <Title>{data.markdownRemark.frontmatter.infos}</Title>
         <Links>
-          <Link to='destructuring/destructuring-objects/'>{data.markdownRemark.frontmatter.destructObj[0].title}</Link>
+          <StyledLink to='destructuring/destructuring-objects/'>{data.markdownRemark.frontmatter.destructObj[0].title}</StyledLink>
         </Links>
       </ContentInfos>
     </Content>
