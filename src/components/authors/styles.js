208cc4a9c0557ee8d76e7e29b0a2f7258e21ddd3
LeonardoCesca <leonardocescaf@gmail.com>
2020-01-27 11:24:22 -0300

    try fix

--- src/components/authors/styles.js
+++ src/components/authors/styles.js
@@ -14,7 +14,7 @@ export const AuthorsContent = styled.div`
         width: 90%;
         background-color: transparent;
         align-items: center;
-        height: 247px;
+        height: 100%;
         margin-top: 35px;
     }
 `;
