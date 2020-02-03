e12fde1f1fad85f307adfc96f8c8582f8a0a77b3
LeonardoCesca <leonardocescaf@gmail.com>
2020-02-03 18:07:23 -0300

    remove: authors component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 import { Content, Title, Paragraph } from './styles';
 
-import Authors from  '../components/authors/index';
-
 export default function Home() {
   return (
     <Content>
       <Title>Home</Title>
-      <Authors />
       <Paragraph> Se você contribuir com o projeto, poderá aparecer aqui também :) </Paragraph>
     </Content>
   )
