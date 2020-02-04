9cd98c4eec995e9726448ae9c3ef4d5ac89d129e
LeonardoCesca <leonardocescaf@gmail.com>
2020-02-04 10:19:54 -0300

    add: favicon

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -22,6 +22,7 @@ const Layout = ({ children }) => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>JavaScript Doc's</title>
+        <link rel="shortcut icon" href="../../../static/favicon.ico" />
       </Helmet>
       <GlobalStyle />
         {isVisible ? (
