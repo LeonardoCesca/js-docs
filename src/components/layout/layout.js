e0300c58899e0c9fe0673bd98829df4c4604d297
LeonardoCesca <leonardocescaf@gmail.com>
2020-02-05 10:56:08 -0300

    add: meta tags (initial format)

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -33,6 +33,12 @@ const Layout = ({ children }) => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>JavaScript Doc's</title>
+        <meta name="description" content="Projeto open-source que tem por objetivo contribuir com a comunidade Javascript." />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://javascript-docs.netlify.com/" />
+        <meta property="og:title" content="Javascript Doc's" />
+        <meta property="og:description" content="Feito em Gatsby com GraphQL." />
       </Helmet>
       <GlobalStyle />
       {isVisible ? (
