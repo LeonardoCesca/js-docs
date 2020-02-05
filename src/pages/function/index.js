a0c211a83c64894dbe5d1c93d26cf1bada55f0cb
brunormferreira <brunormferreira@hotmail.com>
2020-02-05 20:48:51 -0300

    fix functions content and structure

--- src/pages/function/index.js
+++ src/pages/function/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import { graphql } from "gatsby"
 
-import { Content, Title, Paragraph, Links, ContentInfos } from '../styles';
-import { StyledLink } from '../../components/layout/styles';
+import { Content, Title, Paragraph, Links, ContentInfos } from "../styles"
+import { StyledLink } from "../../components/layout/styles"
 
-import Iframes from '../../components/iframes/';
- 
-export default function Function ({data}) {
+import Iframes from "../../components/iframes/"
+
+export default function Function({ data }) {
   return (
     <Content>
-      <Title>
-        {data.markdownRemark.frontmatter.title}
-      </Title>
-      <Paragraph>
-        {data.markdownRemark.frontmatter.subtitle}
-      </Paragraph>
-        <Iframes description="iframe function" url="https://jsfiddle.net/Bruno_Ramires/mhja5r9n/3/embedded/js/" /> 
+      <Title>{data.markdownRemark.frontmatter.title}</Title>
+      <Paragraph>{data.markdownRemark.frontmatter.subtitle}</Paragraph>
+      <Iframes
+        description="iframe function"
+        url="https://jsfiddle.net/Bruno_Ramires/mhja5r9n/3/embedded/js/"
+      />
       <ContentInfos>
+        <Title>Escopo</Title>
+        <Paragraph>{data.markdownRemark.frontmatter.explanation}</Paragraph>
+        <Iframes
+          description="iframe function"
+          url="https://jsfiddle.net/Bruno_Ramires/b65afu81/1/embedded/js/"
+        />
         <Title>{data.markdownRemark.frontmatter.infos}</Title>
         <Links>
-          <StyledLink to='function/function-expression/'>{data.markdownRemark.frontmatter.functionExpression[0].title}</StyledLink>
+          <StyledLink to="function/function-expression/">
+            {data.markdownRemark.frontmatter.functionExpression[0].title}
+          </StyledLink>
         </Links>
       </ContentInfos>
     </Content>
@@ -27,20 +34,18 @@ export default function Function ({data}) {
 }
 
 export const FunctionPageQuery = graphql`
-    query FunctionPageQuery {
-        markdownRemark(fileAbsolutePath: {regex: "/function.md/"}) {
-        frontmatter {
-            title
-            subtitle
-            explanation
-            infos
-            functionExpression {
-            subtitle
-            title
-            }
+  query FunctionPageQuery {
+    markdownRemark(fileAbsolutePath: { regex: "/function.md/" }) {
+      frontmatter {
+        title
+        subtitle
+        explanation
+        infos
+        functionExpression {
+          subtitle
+          title
         }
-        }
+      }
     }
+  }
 `
-
-  
