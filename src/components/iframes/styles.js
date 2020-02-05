0c2cdacc99554cb2c7f9035afa88cfe66014a8f6
brunormferreira <brunormferreira@hotmail.com>
2020-02-05 20:48:03 -0300

    fix iframe border and media query 1350

--- src/components/iframes/styles.js
+++ src/components/iframes/styles.js
@@ -1,13 +1,18 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 export const IframesStyle = styled.iframe`
-    width: 70%;
-    height: 285px;
-    border: none;
-    margin-bottom: 10px;
+  width: 70%;
+  height: 285px;
+  border-radius: 4px;
+  border: none;
+  margin-bottom: 10px;
 
-    @media (max-width: 768px) {
-        width: 90%;
-        height: 100%;
-    }
-`;
+  @media (max-width: 768px) {
+    width: 90%;
+    height: 100%;
+  }
+
+  @media (min-width: 1350px) {
+    width: 40%;
+  }
+`
