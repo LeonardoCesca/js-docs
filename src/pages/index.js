4dd6d549613fce2c5cac6ad7be0ace693248e741
brunormferreira <brunormferreira@hotmail.com>
2020-02-04 21:26:07 -0300

    add paragraph about to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react"
 
-import { Content, Title, Paragraph } from './styles';
+import { Content, Title, Paragraph } from "./styles"
 
-import Contributors from '../components/contributors';
+import Contributors from "../components/contributors"
 
 export default function Home() {
   return (
     <Content>
-      <Title>Home</Title>
-      <Paragraph> Se você contribuir com o projeto, poderá aparecer aqui também :) </Paragraph>
+      <Title>Seja bem vindo ao JavaScript Doc's</Title>
+      <Paragraph>
+        Este projeto foi desenvolvido com a tecnologia
+        <a href="https://www.gatsbyjs.org/"> Gatsby </a>
+        e, possui como intenção contribuir com o Open Source, cooperando e
+        ajudando a aumentar cada vez mais nossa querida comunidade!
+      </Paragraph>
+      <Paragraph>
+        Se você contribuir com o projeto, poderá aparecer aqui também :)
+      </Paragraph>
       <Contributors />
     </Content>
   )
