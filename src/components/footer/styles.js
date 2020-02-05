f07d0229edfe9ae833d96f40f114412416a01d6d
brunormferreira <brunormferreira@hotmail.com>
2020-02-04 21:25:28 -0300

    fix indent

--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,31 +1,31 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 export const FooterContent = styled.footer`
-    position: fixed;
-    bottom: 0;
-    width: 16.67%;
-    border-right: 2px solid #FF9900;
-    background-color: rgb(40, 44, 53);
-    border-top: 2px solid rgb(255, 153, 0);
+  position: fixed;
+  bottom: 0;
+  width: 16.67%;
+  border-right: 2px solid #ff9900;
+  background-color: rgb(40, 44, 53);
+  border-top: 2px solid rgb(255, 153, 0);
+  color: #fff;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
+
+export const FooterText = styled.p`
+  text-align: center;
+  color: #fff;
+
+  & a {
+    margin-top: 15px;
+    margin-bottom: 10px;
     color: #fff;
+    font-weight: bold;
 
-    @media (max-width: 768px) {
-        width: 100%;
+    &:last-child {
+      margin-top: 21px;
     }
-`;
-
-export const FooterText = styled.p` 
-    text-align: center;
-    color: #fff;
-
-    & a {
-        margin-top: 15px;
-        margin-bottom: 10px;
-        color: #fff;
-        font-weight: bold;
-
-        &:last-child {
-            margin-top: 21px;
-        }
-    }
-`;
+  }
+`
--- src/components/iframes/index.js
+++ src/components/iframes/index.js
@@ -1,32 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
-import { ClipLoader } from "react-spinners";
+import { ClipLoader } from "react-spinners"
 
-import { IframesStyle } from './styles';
+import { IframesStyle } from "./styles"
 
-export default function Iframes({url, description}) {
-
-const [isLoader, setIsLoader] = useState(true);
+export default function Iframes({ url, description }) {
+  const [isLoader, setIsLoader] = useState(true)
 
   useEffect(() => {
-      const timer = window.setInterval(() => {
-      setIsLoader(false);
-      }, 1000);
-      return () => {
-      window.clearInterval(timer);
-      };
-  }, []);
+    const timer = window.setInterval(() => {
+      setIsLoader(false)
+    }, 1000)
+    return () => {
+      window.clearInterval(timer)
+    }
+  }, [])
 
   return (
     <>
       {isLoader ? (
-        <ClipLoader
-          size={45}
-          color={"#123abc"}
-          loading={isLoader}
-        />
-        ) : (
-          <IframesStyle title={description} src={url}></IframesStyle>
+        <ClipLoader size={45} color={"#ffa7c4"} loading={isLoader} />
+      ) : (
+        <IframesStyle title={description} src={url} />
       )}
     </>
   )
