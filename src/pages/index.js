dd7112f150e5ac515f3415ee2c9bb26ed4979054
brunormferreira <brunormferreira@hotmail.com>
2020-02-05 21:38:17 -0300

    add blank to anchor links on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,15 @@ export default function Home() {
       <Title>Seja bem vindo ao JavaScript Doc's</Title>
       <Paragraph>
         Este projeto foi desenvolvido com a tecnologia
-        <a href="https://www.gatsbyjs.org/"> Gatsby </a> e com a cooperação do
-        <a href="https://jsfiddle.net/"> JSFiddle</a>. Com a intenção de
-        contribuir com o Open Source, cooperando e ajudando a aumentar cada vez
-        mais nossa querida comunidade!
+        <a href="https://www.gatsbyjs.org/" target="_blank" rel="noreferrer">
+          Gatsby
+        </a>
+        e com a cooperação do
+        <a href="https://jsfiddle.net/" target="_blank" rel="noopener">
+          JSFiddle
+        </a>
+        . Com a intenção de contribuir com o Open Source, cooperando e ajudando
+        a aumentar cada vez mais nossa querida comunidade!
       </Paragraph>
       <Paragraph>
         Se você contribuir com o projeto, poderá aparecer aqui também :)
