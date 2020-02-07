6e85edabb30789fb9cce23d73d52d39289f9c8ea
soto92 <mauriciosoto@rede.ulbra.br>
2020-02-07 11:03:59 -0300

    removed timeout

--- src/components/iframes/index.js
+++ src/components/iframes/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 
 import { ClipLoader } from "react-spinners"
 
@@ -7,22 +7,14 @@ import { IframesStyle } from "./styles"
 export default function Iframes({ url, description }) {
   const [isLoader, setIsLoader] = useState(true)
 
-  useEffect(() => {
-    const timer = window.setInterval(() => {
-      setIsLoader(false)
-    }, 1000)
-    return () => {
-      window.clearInterval(timer)
-    }
-  }, [])
-
   return (
     <>
-      {isLoader ? (
-        <ClipLoader size={45} color={"#ffa7c4"} loading={isLoader} />
-      ) : (
-        <IframesStyle title={description} src={url} />
-      )}
+      <ClipLoader size={45} color={"#ffa7c4"} loading={isLoader} />
+      <IframesStyle
+        onLoad={() => setIsLoader(false)}
+        title={description}
+        src={url}
+      />
     </>
   )
 }
