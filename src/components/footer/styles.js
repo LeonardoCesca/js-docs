103282c292b7c8af67cd5de64b0315fa8268ab2e
brunormferreira <brunormferreira@hotmail.com>
2020-02-11 21:49:02 -0300

    fix footer styles

--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,10 +1,11 @@
 import styled from "styled-components"
 
 export const FooterContent = styled.footer`
-  width: 100%;
   padding: 1rem;
   background-color: rgb(40, 44, 53);
   color: #fff;
+  border-top: 2px solid rgb(255, 153, 0);
+  width: 83%;
 `
 
 export const FooterText = styled.p`
