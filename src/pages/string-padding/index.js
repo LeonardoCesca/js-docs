c78011aade824f5f56cb94ae0f386c3881a9d7c6
Gabriel Neri <neri.gabriel78@gmail.com>
2020-03-16 11:03:39 -0300

    Add: identação nos arquivos de index e layout e ajustes nos https dos links

--- src/pages/string-padding/index.js
+++ src/pages/string-padding/index.js
@@ -22,33 +22,33 @@ export default function StringPadding({ data }) {
       <Paragraph>{subtitle}</Paragraph>
       <Iframes
         description="how to use string padding"
-        url="https://jsfiddle.net/neriGabriel/Lszm5w7y/5/embedded/js/"
+        url="https://jsfiddle.net/neriGabriel/06cy24be/1/embedded/js/"
       />
       <Subtitle>Explicação</Subtitle>
       <Paragraph>
         Os comandos padStart e padEnd são comumente utilizados em strings e sempre esperam no primeiro parametro um tamanho
-		e caso esse tamanho for menor que tamanho da string
-		nada é feito, caso seja maior é adionado um espaço(ou um valor pré-definido) no inicio ou fim respectivamente.
+		    e caso esse tamanho for menor que tamanho da string
+		    nada é feito, caso seja maior é adionado um espaço(ou um valor pré-definido) no inicio ou fim respectivamente.
       </Paragraph>
       <Iframes
         description="how to use string padding 2 "
-        url="https://jsfiddle.net/neriGabriel/Lszm5w7y/31/embedded/js/"
+        url="https://jsfiddle.net/neriGabriel/06cy24be/2/embedded/js/"
       />
       <Iframes
         description="how to use string padding 3 "
-        url="https://jsfiddle.net/neriGabriel/Lszm5w7y/35/embedded/js/"
+        url="https://jsfiddle.net/neriGabriel/06cy24be/4/embedded/js/"
       />
       <Iframes
         description="how to use string padding 4 "
-        url="https://jsfiddle.net/neriGabriel/Lszm5w7y/38/embedded/js/"
-      />	
+        url="https://jsfiddle.net/neriGabriel/06cy24be/6/embedded/js/"
+      />
       <Subtitle>Exemplo de preenchimento de uma string com String padding</Subtitle>
       <Paragraph>
-        A baixo um exemplo prático de um simples preenchimento de uma string com 15 caracteres (numeros randomicos) no começo e fim 
+        A baixo um exemplo prático de um simples preenchimento de uma string com 15 caracteres (numeros randomicos) no começo e fim
       </Paragraph>
       <Iframes
         description="how to use string padding 5"
-        url="//jsfiddle.net/neriGabriel/Lszm5w7y/59/embedded/js/"
+        url="https://jsfiddle.net/neriGabriel/06cy24be/8/embedded/js/"
       />
     </Content>
   )
